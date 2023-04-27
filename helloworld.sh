0fb4fa7d65a02f92557cca4ab07d7828d8165991
rkcee <karki35ree@gmail.com>
2023-04-27 01:45:48 +0000

    this is my commit

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
